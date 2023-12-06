fbf045869d4e25adf34be3fae99cc5cb9f1eb445
JiteshBalani <balani.jitesh911@gmail.com>
2023-12-07 04:24:16 +0530

    GET, POST  and PUT operations.

---
+++ 03-Rest+MongoDB/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+const data = JSON.parse(fs.readFileSync("data.json"));
+
+const products = data.products;
+
+//GET Operations
+app.get("/home", (_,res) => {
+    res.send("Hello, World!");
+});
+
+app.get("/products", (_,res) => {
+    res.send(products);
+});
+
+app.get("products/:productId", (req,res) => {
+    const productId = Number(req.params.productId);
+    const product = products.find(product => product.id === productId) || {};
+    if(product.id) {
+        res.send(product);
+    } else {
+        res.status(404).send("Product Not Found");
+    }
+});
+
+// POST Operations
+app.post("/product", (req, res) => {
+    products.push(req.body);
+    res.status(201).send("Successfully added");
+});
+
+//PUT Operations
+app.put("products/:productId", (req,res) => {
+    const productId = Number(req.params.productId);
+    const productIdx = products.findIndex(product => product.id === productId);
+    products.splice(productIdx, 1, { ...req.body, id: productId});
+    res.status(201).send("Successfully Updated!");
+});
+
+app.listen(8080, ()=> {
+    console.log("Server started. You can access it on http://localhost:8080/")
+});
