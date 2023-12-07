df4e4b7de48e1cedf3a9d3a5861e70f3b6168b35
JiteshBalani <balani.jitesh911@gmail.com>
2023-12-08 04:59:58 +0530

    comment

--- 03-Rest+MongoDB/test.js
+++ 03-Rest+MongoDB/test.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const db = "mongodb+srv://balanijitesh911:pcc228AoxIMCAR68@cluster0.6xovpma.mongodb.net/?retryWrites=true&w=majority";
+// const db = "mongodb+srv://balanijitesh911:pcc228AoxIMCAR68@cluster0.6xovpma.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose
     .connect(db, {})
