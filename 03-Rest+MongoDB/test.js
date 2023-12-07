6a9ab7e96d4276e4c3630896357294f917121a7a
JiteshBalani <balani.jitesh911@gmail.com>
2023-12-08 04:53:05 +0530

    MongoDB Setup and Mongoose

--- 03-Rest+MongoDB/test.js
+++ 03-Rest+MongoDB/test.js
@@ -1,1 +1,12 @@
+const mongoose = require("mongoose");
+
 const db = "mongodb+srv://balanijitesh911:pcc228AoxIMCAR68@cluster0.6xovpma.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+    .connect(db, {})
+    .then (() => {
+        console.log("Connection Successful");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
